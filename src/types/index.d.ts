c5a74b2903ec894c16c235eccc1e8c19d56b8909
Kamal Tufekcic <kamal.tufekcic@ugs.ro>
2025-10-04 06:43:30 +0300

    initial commit

--- src/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview TypeScript definitions for @openforge-sh/liboqs-node
- * @module @openforge-sh/liboqs-node
- */
-
-// Re-export everything from main module types
-export type * from './algorithms.js';
-export * from './algorithms.js';
-export * from './errors.js';
